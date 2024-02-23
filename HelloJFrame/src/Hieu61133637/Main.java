b966bb2ca74f8ee30b72328cc25276dad3b7c9f4
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-02-23 14:35:37 +0700

    Làm quen với JFrame

---
+++ HelloJFrame/src/Hieu61133637/Main.java
@@ -0,0 +1,15 @@
+package Hieu61133637;
+import  javax.swing.JFrame;
+import javax.swing.WindowConstants;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame("123");
+		f.setTitle("Vi du ve JFrame");
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setSize(200,200);
+		f.setVisible(true);
+	}
+
+}
