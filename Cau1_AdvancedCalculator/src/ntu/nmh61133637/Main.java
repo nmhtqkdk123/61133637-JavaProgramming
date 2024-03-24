3d4cdabeb2d6a54837190da1c2cda7015e9df9f5
Ngô Minh Hiếu <102578629+nmhtqkdk123@users.noreply.github.com>
2024-03-24 11:08:18 +0700

    Cau1_ThiGiuaKy
    Cau1_ThiGiuaKy

---
+++ Cau1_AdvancedCalculator/src/ntu/nmh61133637/Main.java
@@ -0,0 +1,10 @@
+package ntu.nmh61133637;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainScreen().setVisible(true);
+	}
+
+}
---
+++ Cau1_AdvancedCalculator/src/ntu/nmh61133637/MainScreen.java
@@ -0,0 +1,24 @@
+package ntu.nmh61133637;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainScreen extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public MainScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
